96ee193292e6134cadab1569b5debc407b88afaa
ddayan0 <denday198@gmail.com>
2021-04-09 08:57:00 -0400

    added source code and binary

---
+++ tqof.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	char ans1[50]; // could be less
+	char ans2[50]; // could be less
+	char ans3[50]; // could be less
+	printf("How many cities are there in South Africa?: \n");
+	scanf("%s", ans1);
+	printf("What is 4 + 5: \n");
+	scanf("%s", ans2);
+	printf("What is (9 * 5 * 8) - 3231: \n");
+	scanf("%s", ans3);
+	//printf("You entered: %s %s %s", ans1, ans2, ans3); testing
+	if(!strcmp(ans1, ans2)){
+		if(!strcmp(ans2, ans3)){
+			printf("cmJyY3Rme2lfdGgwdWdodF9iaW4xNHIxZXNfdzNyM196M3Iwc180bmRfMXN9\n");
+		}
+	}
+	return 0;
+
+}
